6534781b2d9bcd16657403ed66356d9e6acaebfc
Dima Batalshikov <batalshikov.d@gmail.com>
2024-08-12 20:47:24 +0300

    fix partytown hints

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://DamianDream.github.io',
   base: 'charity',
-  integrations: [tailwind(), sitemap(), partytown()]
+  compressHTML: true,
+  integrations: [tailwind(), sitemap()],
 });
